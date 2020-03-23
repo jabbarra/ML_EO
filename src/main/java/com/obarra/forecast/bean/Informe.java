3360dc5304e688eb7f5045b09b4a6aa661c52935
obarra <barra_omar@hotmail.com>
2020-03-23 17:54:17 -0300

    spotbugs RCN_REDUNDANT_NULLCHECK_OF_NONNULL_VALUE

--- src/main/java/com/obarra/forecast/bean/Informe.java
+++ src/main/java/com/obarra/forecast/bean/Informe.java
@@ -1,23 +1,10 @@
 package com.obarra.forecast.bean;
 
 import lombok.Data;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class Informe {
     private String titulo;
     private List<Periodo> listaPeriodos;
-
-    /**
-     * @return the listaPeriodos
-     */
-    public List<Periodo> getListaPeriodos() {
-        if (this.listaPeriodos == null) {
-            this.listaPeriodos = new ArrayList<>();
-        }
-        return listaPeriodos;
-    }
-
 }
