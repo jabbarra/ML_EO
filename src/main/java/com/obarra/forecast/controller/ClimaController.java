4460f21b2cb935ac43638bd9ac41e9d0103e21b5
obarra <barra_omar@hotmail.com>
2020-03-24 14:20:52 -0300

    use getmapping

--- src/main/java/com/obarra/forecast/controller/ClimaController.java
+++ src/main/java/com/obarra/forecast/controller/ClimaController.java
@@ -6,10 +6,9 @@ import com.obarra.forecast.bean.Informe;
 import com.obarra.forecast.service.ClimaService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,29 +25,28 @@ public class ClimaController {
         this.climaService = climaService;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    @ResponseBody
+    @GetMapping
     public ClimaEstado getClimaByDia(
             final @RequestParam(value = "dia", required = true) Long dia) {
         return climaService.getClimaDelDia(dia);
     }
 
-    @RequestMapping(value = "/climas", method = RequestMethod.GET)
+    @GetMapping(value = "/weathers")
     public List<Clima> getClimas() {
         return climaService.getClimas();
     }
 
-    @RequestMapping(value = "/sequias", method = RequestMethod.GET)
+    @GetMapping(value = "/droughts")
     public Informe getSequias() {
         return climaService.getPeriodosSequia();
     }
 
-    @RequestMapping(value = "/lluvias", method = RequestMethod.GET)
+    @GetMapping(value = "/rains")
     public Informe getLLuvias() {
         return climaService.getPeriodoLLuvia();
     }
 
-    @RequestMapping(value = "/ideales", method = RequestMethod.GET)
+    @GetMapping(value = "/idealDays")
     public Informe getIdeales() {
         return climaService.getCondicionesOptimas();
     }
