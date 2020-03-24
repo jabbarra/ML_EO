fce696de8002caf4019270b3890d5ea6f5512217
obarra <barra_omar@hotmail.com>
2020-03-24 16:55:09 -0300

    adjust junit5 and spring boot 2.1

--- src/test/java/com/obarra/forecast/ApplicationTests.java
+++ src/test/java/com/obarra/forecast/ApplicationTests.java
@@ -1,12 +1,12 @@
 package com.obarra.forecast;
 
-
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class ApplicationTests {
 
     @Test
     public void contextLoads() {
     }
-
 }
