5204d30b8793f2c009fdc353d125c16e6aeccc29
obarra <barra_omar@hotmail.com>
2020-04-11 16:58:37 -0300

    integration test

--- src/test/java/com/obarra/forecast/ApplicationTests.java
+++ src/test/java/com/obarra/forecast/ApplicationTests.java
@@ -2,31 +2,24 @@ package com.obarra.forecast;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.obarra.forecast.bean.Weather;
-import com.obarra.forecast.controller.ForecastController;
-import com.obarra.forecast.service.ForecastService;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 
 @SpringBootTest
@@ -38,27 +31,50 @@ public class ApplicationTests {
 
     @Test
     public void contextLoads() throws Exception {
+        mockMvc.perform(get("/weather?day=566"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.day").value(5665L));
+
+        mockMvc.perform(get("/forecast/rain"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.weather.id").value(1L))
+                .andExpect(jsonPath("$.maximumIntensityDay").value(3348L));
+
+        mockMvc.perform(get("/forecast/{name}", "optimum"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.weather.id").value(3L));
+
+        mockMvc.perform(get("/forecast/{name}", "drought"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.weather.id").value(2L));
+
+
         Path resourceDirectory = Paths.get("src","test","resources", "weathers.json");
-
-
         ObjectMapper objectMapper = new ObjectMapper();
         List<Weather> object = objectMapper.readValue(
                 new String(Files.readAllBytes(resourceDirectory.toRealPath())
                 ),
                 objectMapper.getTypeFactory().constructCollectionType(List.class, Weather.class)
         );
-        System.out.println(object);
 
         String bodyString = mockMvc.perform(get("/weather/list")
                 .contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk())
                 .andExpect(content()
                         .contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$", Matchers.hasSize(3)))
+                .andExpect(jsonPath("$[1].name", Matchers.is("Optimum")))
                 .andReturn().getResponse().getContentAsString();
 
-        List<Weather>  response = objectMapper.readValue(bodyString, objectMapper.getTypeFactory().constructCollectionType(List.class, Weather.class)
-        );
-
+        List<Weather> response = objectMapper
+                .readValue(bodyString,
+                        objectMapper
+                                .getTypeFactory()
+                                .constructCollectionType(List.class, Weather.class));
         Assertions.assertIterableEquals(object, response);
     }
 }
